66b6bd0e213cd71e0374fd26db6c50d89dce99a6
hwccc <342786709@qq.com>
2020-05-14 11:03:34 +0800

    增加底层通信授权

--- andromeda/src/main/java/org/qiyi/video/svg/config/DispatcherConstants.java
+++ andromeda/src/main/java/org/qiyi/video/svg/config/DispatcherConstants.java
@@ -6,8 +6,20 @@ public class DispatcherConstants {
 
     public static final String URI_SUFFIX = "qiyi.svg.dispatcher";
 
+    /**
+     * 默认授权
+     */
+    public static String AUTHORITY_DEFAULT = "com.hwc.authority.andromeda";
+
+    /**
+     * 语音授权
+     */
     public static String AUTHORITY_SPEECH = "com.hwc.authority.speech";
 
-    public static String AUTHORITY_DEFAULT = "com.hwc.authority.andromeda";
+    /**
+     * 底层通信授权
+     */
+    public static String AUTHORITY_UNDERLYING_COMMUNICATION = "com.hwc.authority.underlying.communication";
+
 
 }
